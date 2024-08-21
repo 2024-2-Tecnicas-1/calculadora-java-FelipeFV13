67b425748d4f8fcba6576cae98af5cc53e5f4e58
User <user1@email.com>
2024-08-20 19:23:10 -0500

    Auto stash before merge of "main" and "origin/main"
    se suben los casos case de modulo y potencia

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -26,8 +26,15 @@ public class Calculadora {
                 // Operación división:
                 respuesta = (double) numero1 / numero2;
                 break;
-            
             // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
+            case '^' :
+                respuesta = (double) Math.pow(numero1, numero2);
+                break;
+            case '%' :
+                respuesta = (double) numero1 % numero2;
+                break;
+            
+            
                 
             default:
                 throw new IllegalArgumentException("Operación inválida.");
